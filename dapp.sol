2a0722467aac6cbe595d954412ce239b3148ace6
Prajwal01221 <105379339+Prajwal01221@users.noreply.github.com>
2022-05-21 15:33:17 +0530

    Update dapp.sol

--- dapp.sol
+++ dapp.sol
@@ -1,24 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity  >=0.7.0 <0.9.0;
 contract PersonalDetails {
+ mapping(string=>Aadhardetails)details;
     struct Aadhardetails {
         string name;
         string gender;
-        uint DateofBirth;
+        string DateofBirth;
         uint aadharnumber;
     }
     
     Aadhardetails[] userdetails;
 
- function storeAadhardetails(string memory _name, uint _DateofBirth, string memory _gender, uint _aadharnumber) private{
-     userdetails.push(Aadhardetails(_name, _gender, _DateofBirth, _aadharnumber));
+ function storeAadhardetails(string memory _name, string memory _DateofBirth, string memory _gender, uint _aadharnumber) public {
+     details[_name].name=_name;
+     details[_name].gender=_gender;
+     details[_name].DateofBirth=_DateofBirth;
+     details[_name].aadharnumber=_aadharnumber;
     }
-    
-    
-    function storewalletaddress(string memory _publickey) private{
+    function getdetails(string memory _name)public view returns(string memory,string memory,string memory,uint){
+       return(details[_name].name,details[_name].gender,details[_name].DateofBirth,details[_name].aadharnumber); 
         
     }
-
-
-
+    
 }
